5ed885f9104b8f811b2416175db95b4e26e33ca1
dchancia <dchancia@purdue.edu>
2021-11-28 22:25:22 -0500

    Update code

--- Detection/Models/detection_SVM.py
+++ Detection/Models/detection_SVM.py
@@ -11,8 +11,9 @@ from collections import Counter
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.utils import shuffle
 from sklearn import svm, metrics
+from sklearn.metrics import RocCurveDisplay, auc
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
 from sklearn.feature_selection import SelectFromModel
 from sklearn.preprocessing import StandardScaler
 import warnings
@@ -58,6 +59,9 @@ ros = RandomOverSampler(random_state=42)
 X_bal, y_bal = ros.fit_resample(X_reduced, np.array(y))
 X_bal = StandardScaler().fit_transform(X_bal)
 
+# X_bal = X_reduced
+# y_bal = np.array(y)
+
 # Print
 toprint.append("5. Classes distribution before balancing:")
 toprint.append(y.value_counts())
@@ -68,13 +72,86 @@ toprint.append(Counter(y_bal))
 X_train, X_test, y_train, y_test = train_test_split(X_bal, y_bal, test_size=0.2, random_state=42)
 
 # Train SVM
-svm_model = svm.SVC()
+svm_model = svm.SVC(kernel="linear", probability=True, random_state=42)
 svm_model.fit(X_train, y_train)
 
 # Evaluate model
 y_hat = svm_model.predict(X_test)
 
+# Metrics
+acc = metrics.accuracy_score(y_test, y_hat)
+f1 = metrics.f1_score(y_test, y_hat, average='macro')
+precision = metrics.precision_score(y_test, y_hat, average='macro')
+recall = metrics.recall_score(y_test, y_hat, average='macro')
+
+#Print
+toprint.append("7. Accuracy: {}".format(acc))
+toprint.append("8. Precision: {}".format(precision))
+toprint.append("9. Recall: {}".format(recall))
+toprint.append("10. F1-score: {}".format(f1))
+
+# Evaluate model CV
+# Run classifier with cross-validation and plot ROC curves
+cv = StratifiedKFold(n_splits=5)
+classifier = svm.SVC(kernel="linear", probability=True, random_state=42)
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+fig, ax = plt.subplots()
+for i, (train, test) in enumerate(cv.split(X_train, y_train)):
+    classifier.fit(X_train[train], y_train[train])
+    viz = RocCurveDisplay.from_estimator(
+        classifier,
+        X_train[test],
+        y_train[test],
+        name="ROC fold {}".format(i),
+        alpha=0.3,
+        lw=1,
+        ax=ax,
+    )
+    interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+ax.plot(
+    mean_fpr,
+    mean_tpr,
+    color="b",
+    label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+    lw=2,
+    alpha=0.8,
+)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax.fill_between(
+    mean_fpr,
+    tprs_lower,
+    tprs_upper,
+    color="grey",
+    alpha=0.2,
+    label=r"$\pm$ 1 std. dev.",
+)
+
+ax.set(
+    xlim=[-0.05, 1.05],
+    ylim=[-0.05, 1.05],
+    title="Receiver operating characteristic example",
+)
+ax.legend(loc="lower right")
 
 # Write file
 for elem in toprint:
     print(elem)
+
+plt.show()
